4c8590b6382e9b3df0a978997e74f7c36eee5f3d
meitantei69 <66460248+meitantei69@users.noreply.github.com>
2020-12-28 22:56:20 +0800

    Update feedback.js

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,17 +1,27 @@
 
-function validate()  {
-  var a = document.forms["feedback_form"]["name"].value;
-  var b = document.forms["feedback_form"]["email"].value;
-  var c = document.forms["feedback_form"]["subject"].value;
-  var d = document.forms["feedback_form"]["message"].value;
-  // do {
-  //   location.assign("thank_you.html");
-  // } while (a == "" || b == "" || c == "" || d == "");
-  if(a == "" || b == "" || c == "" || d == "") {
-    alert("Every text box must be filled. ");
-    return false;
+// function validate()  {
+//   var a = document.forms["feedback_form"]["name"].value;
+//   var b = document.forms["feedback_form"]["email"].value;
+//   var c = document.forms["feedback_form"]["subject"].value;
+//   var d = document.forms["feedback_form"]["message"].value;
+//   // do {
+//   //   location.assign("thank_you.html");
+//   // } while (a == "" || b == "" || c == "" || d == "");
+//   if(a == "" || b == "" || c == "" || d == "") {
+//     alert("Every text box must be filled. ");
+//     return false;
+//   }
+//   else {
+//     location.href("thank_you.html");
+//   }
+// }
+
+function validate() {
+  if (window.confirm("Click okay to continue. ")) {
+    alert('message')
+    window.location = "thank_you.html"
   }
   else {
-    location.href("thank_you.html");
+    die();
   }
 }
