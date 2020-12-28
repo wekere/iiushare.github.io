64acd72096c7921b467c11caa3c1e3f80afb5191
AriefYolo <76680376+AriefYolo@users.noreply.github.com>
2020-12-28 21:54:54 +0800

    Update PrintedMaterials.js

--- scripts/PrintedMaterials.js
+++ scripts/PrintedMaterials.js
@@ -1,15 +1,3 @@
-<style>
-.btn {
-  background-color: #f4511e;
-  border: none;
-  color: white;
-  padding: 16px 32px;
-  text-align: center;
-  font-size: 16px;
-  margin: 4px 2px;
-  opacity: 0.6;
-  transition: 0.3s;
+function myFunction() {
+  alert("I am an alert box!");
 }
-
-.btn:hover {opacity: 1}
-</style>
