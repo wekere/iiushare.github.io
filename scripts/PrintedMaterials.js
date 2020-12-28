6a9c632cb972beb1c9caa156823387d4d1a371db
AriefYolo <76680376+AriefYolo@users.noreply.github.com>
2020-12-28 21:19:21 +0800

    Update PrintedMaterials.js

--- scripts/PrintedMaterials.js
+++ scripts/PrintedMaterials.js
@@ -5,6 +5,6 @@ $(document).ready(function()
     $("#material1").fadeIn(2000);
     $("#material2").fadeIn(2500);
     $("#material3").fadeIn(3000);
-	$("#material4").fadeIn(3500);
+    $("#material4").fadeIn(3500);
   });
 </script>
