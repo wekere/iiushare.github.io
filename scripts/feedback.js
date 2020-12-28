df1c59d69cbc244233032b6bcc001b344876cd2b
meitantei69 <66460248+meitantei69@users.noreply.github.com>
2020-12-28 21:24:39 +0800

    Update feedback.js

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,18 +1,18 @@
 
 function validate()  {
-  var a = document.forms["feedback_form"]["name"].value;
-  var b = document.forms["feedback_form"]["email"].value;
-  var c = document.forms["feedback_form"]["subject"].value;
-  var d = document.forms["feedback_form"]["message"].value;
-  // do {
-  //   location.assign("thank_you.html");
-  // } while (a == "" || b == "" || c == "" || d == "");
-  if(a == "" || b == "" || c == "" || d == "") {
-    alert("Every text box must be filled. ");
-    return false;
-  }
-  else {
+  // var a = document.forms["feedback_form"]["name"].value;
+  // var b = document.forms["feedback_form"]["email"].value;
+  // var c = document.forms["feedback_form"]["subject"].value;
+  // var d = document.forms["feedback_form"]["message"].value;
+  do {
     location.assign("thank_you.html");
-    return true;
-  }
+  } while (a == "" || b == "" || c == "" || d == "");
+  // if(a == "" || b == "" || c == "" || d == "") {
+  //   alert("Every text box must be filled. ");
+  //   return false;
+  // }
+  // else {
+  //   location.assign("thank_you.html");
+  //   return true;
+  // }
 }
