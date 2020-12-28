ffd7fe8767046471acd589ec09b163005ada11c2
meitantei69 <66460248+meitantei69@users.noreply.github.com>
2020-12-28 21:20:34 +0800

    Update feedback.js

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -4,12 +4,15 @@ function validate()  {
   var b = document.forms["feedback_form"]["email"].value;
   var c = document.forms["feedback_form"]["subject"].value;
   var d = document.forms["feedback_form"]["message"].value;
+  // do {
+  //   location.assign("thank_you.html");
+  // } while (a == "" || b == "" || c == "" || d == "");
   if(a == "" || b == "" || c == "" || d == "") {
     alert("Every text box must be filled. ");
     return false;
   }
   else {
-    location.assign("thank-you.html");
+    location.assign("thank_you.html");
     return true;
   }
 }
