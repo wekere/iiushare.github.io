c84fa12d33a594a8564358c9c1045e52f0e60a30
meitantei69 <66460248+meitantei69@users.noreply.github.com>
2020-12-28 21:01:27 +0800

    Update feedback.js

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,14 +1,8 @@
-function confirm()  {
-  alert("Thank you for your feedback!")
-}
 
 function validate()  {
   var x = document.forms["feedback_form"]["name"]["email"]["subject"]["message"].value;
   if(x == "") {
     alert("All text boxes cannot be empty. ");
     return false;
-  }
-  else {
-    confirm();
   }
 }
